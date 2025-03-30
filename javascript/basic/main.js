5851a3eda774433be5abae5a92af9e402e657f42
Ricardo Fragoso <ricardofsilva0412@gmail.com>
2025-03-30 20:49:54 -0300

    entendendo map, filter, reduce; e aprendendo a usar o DOM em javascript e algumas interações com o usuario

--- javascript/basic/main.js
+++ javascript/basic/main.js
@@ -1,1 +1,19 @@
-const numbers = [1,2,3,4,5];
+const numbers = [1, 2, 3, 4, 5];
+const numbersMultiplyByTwo = numbers.map(function(number){
+    return number * 2
+
+})
+const ages = [8, 13, 27, 30, 22, 40];
+
+const evenAges = ages.filter(function(age){
+    return age % 2 === 0
+
+});
+
+const ages2 = [8, 13, 27, 30, 22, 40];
+const sumOfAges = ages2.reduce(function(age, accumalator){
+    return accumalator + age
+    
+}, 0)
+
+console.log(sumOfAges)
